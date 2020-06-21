387690f2b0a8b4a4bc849d39af2e5575f4637883
akhil sarwar <akhilsarwar24@gmail.com>
2020-06-21 17:30:32 +0530

    added backtrack_algo.py

---
+++ backtrack_algo.py
@@ -0,0 +1,113 @@
+#Implementing Backtracking algorithm
+
+import numpy as np
+n=20
+a=np.zeros([n,n])
+status = True
+occupied=[]
+
+
+#function to implement backtracking
+def Backtracking(column, status):
+
+	for row in range (n):
+		a[row][column]=1
+
+		#checking whether a conflict happens
+		for i in range(len(occupied)):
+			if row!=occupied[i][0] and (abs(occupied[i][0]-row)!=abs(occupied[i][1]-column)):
+				status=True
+			else:
+				status=False
+				break
+
+
+		if status==True:
+			if column<n-1:
+				occupied.append([row,column])
+				if Backtracking(column+1, status):
+					return True
+				else:
+					a[row][column]=0
+					occupied.pop(-1)
+					
+			else:
+				return True
+		else:
+			a[row][column]=0
+			
+
+		
+	return False
+
+
+		
+
+Decision=Backtracking(0, status)
+if Decision==True:
+	print(a)
+else:
+	print(False)
+
+
+
+
+
+
+
+# # Python code to for n Queen placement 
+# class GfG: 
+# 	def __init__(self): 
+# 		self.MAX = 10
+# 		self.arr = [0] * self.MAX
+# 		self.no = 0
+
+# 	def breakLine(self): 
+# 		print("\n------------------------------------------------") 
+
+# 	def canPlace(self, k, i): 
+		
+# 		# Helper Function to check 
+# 		# if queen can be placed 
+# 		for j in range(1, k): 
+# 			if (self.arr[j] == i or
+# 			(abs(self.arr[j] - i) == abs(j - k))): 
+# 				return False
+# 		return True
+
+# 	def display(self, n): 
+		
+# 		# Function to display placed queen 
+# 		self.breakLine() 
+# 		self.no += 1
+# 		print("Arrangement No.", self.no, end = " ") 
+# 		self.breakLine() 
+
+# 		for i in range(1, n + 1): 
+# 			for j in range(1, n + 1): 
+# 				if self.arr[i] != j: 
+# 					print("\t_", end = " ") 
+# 				else: 
+# 					print("\tQ", end = " ") 
+# 			print() 
+
+# 		self.breakLine() 
+
+# 	def nQueens(self, k, n): 
+		
+# 		# Function to check queens placement 
+# 		for i in range(1, n + 1): 
+# 			if self.canPlace(k, i): 
+# 				self.arr[k] = i 
+# 				if k == n: 
+# 					self.display(n) 
+# 				else: 
+# 					self.nQueens(k + 1, n) 
+
+# # Driver Code 
+# if __name__ == '__main__': 
+# 	n = 9
+# 	obj = GfG() 
+# 	obj.nQueens(1, n) 
+
+# # This code is contributed by vibhu4agarwal 
---
+++ n_queen.py
@@ -0,0 +1,7 @@
+import backtrack_algo
+import pygame as pg
+import numpy as np
+
+class Grid:
+    def __init__(self, n, ):
+        self.n=n
